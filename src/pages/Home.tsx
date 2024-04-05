ef45540347c68b9d798e25d4ad3f43deebc96f87
FatimaPaluga13 <palugafatima1@gmail.com>
2024-04-05 20:45:13 +0800

    home changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,77 @@
+import React from 'react';
 import {
-    //Initial Components
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonTextarea,
+  IonButton,
+  IonIcon,
+  IonFooter,
+  IonText,
 } from '@ionic/react';
+import { camera, videocam, images, notifications, chatbubbles, person } from 'ionicons/icons';
+import './Home.css';
 
-// CSS
-//import './profile.css';
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="primary">
+          <IonTitle>Home</IonTitle>
+          <IonButton color="light" slot="end">
+            <IonIcon icon={chatbubbles} />
+          </IonButton>
+          <IonButton color="light" slot="end">
+            <IonIcon icon={notifications} />
+          </IonButton>
+          <IonButton color="light" slot="end">
+            <IonIcon icon={person} />
+          </IonButton>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        {/* Post Section */}
+        <IonList>
+          {/* Post Status */}
+          <IonItem>
+            <IonAvatar slot="start">
+            <img alt="Silhouette of mountains" id="profile_pic" src="../src/assets/img/PROFPIC_FATIMA_PNG.png" />
+            </IonAvatar>
+            <IonTextarea autoGrow placeholder="What's on your mind?" />
+          </IonItem>
+          {/* End Post Status */}
 
-  const Home: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Home</IonTitle>
+          {/* Add more posts here */}
+
+        </IonList>
+
+        {/* Photos, Videos, and Reels Section */}
+        {/* <IonFooter>
+          <IonToolbar color="primary">
+            <IonButton color="light" slot="start">
+              <IonIcon icon={camera} />
+              <IonText>Photos</IonText>
+            </IonButton>
+            <IonButton color="light" slot="start">
+              <IonIcon icon={videocam} />
+              <IonText>Videos</IonText>
+            </IonButton>
+            <IonButton color="light" slot="start">
+              <IonIcon icon={images} />
+              <IonText>Reels</IonText>
+            </IonButton>
           </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Home</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <h1>Hello World!<br></br>This is my homepage.</h1>
-        </IonContent>
-      </IonPage>
-    );
-  };
+        </IonFooter> */}
+        
 
-  //
-  export default Home;
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
