5516f63e51585bc378b833cb72f6f00b4eb1274f
FatimaPaluga13 <palugafatima1@gmail.com>
2024-04-05 20:09:05 +0800

    finalizing

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,20 +1,7 @@
-// Tab2.tsx
-
-import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonLabel } from '@ionic/react';
-import './Tab2.css';
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
 const Tab2: React.FC = () => {
-  const [count, setCount] = useState(0);
-
-  const incrementCounter = () => {
-    setCount(count + 1);
-  };
-
-  const resetCounter = () => {
-    setCount(0);
-  };
-
   return (
     <IonPage>
       <IonHeader>
@@ -22,16 +9,8 @@ const Tab2: React.FC = () => {
           <IonTitle>Click Counter</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-        <div className="container">
-          <div className="counter-container">
-            <IonLabel className="counter-label">Count: {count}</IonLabel>
-          </div>
-          <div className="button-container">
-            <IonButton onClick={incrementCounter}>Click to Increment</IonButton>
-            <IonButton onClick={resetCounter}>Reset</IonButton>
-          </div>
-        </div>
+      <IonContent>
+        {/* Your content here */}
       </IonContent>
     </IonPage>
   );
