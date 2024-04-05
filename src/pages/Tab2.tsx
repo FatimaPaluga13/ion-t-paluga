6f7ee72fc703c02a303f7ec5bb546f227d3810e5
FatimaPaluga13 <palugafatima1@gmail.com>
2024-04-05 20:12:18 +0800

    Adding content for my clickcounter tab

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+// Tab2.tsx
+
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonLabel } from '@ionic/react';
+import './Tab2.css';
 
 const Tab2: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
+
+  const resetCounter = () => {
+    setCount(0);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -9,8 +22,16 @@ const Tab2: React.FC = () => {
           <IonTitle>Click Counter</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        {/* Your content here */}
+      <IonContent className="ion-padding">
+        <div className="container">
+          <div className="counter-container">
+            <IonLabel className="counter-label">Count: {count}</IonLabel>
+          </div>
+          <div className="button-container">
+            <IonButton onClick={incrementCounter}>Click to Increment</IonButton>
+            <IonButton onClick={resetCounter}>Reset</IonButton>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
